7839154b831eca5da37d10e4852085b6d3db97d1
Richard Fenix <richard.fenix@gmail.com>
2018-02-21 05:02:09 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,3 +5,7 @@ function updatePlaylist(playlist, artist, song) {
   return Object.assign({}, playlist, {[artist]: song})
 }
 
+function removeFromPlaylist(playlist, artist){
+  delete playlist[artist];
+  return playlist;
+}
