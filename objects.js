6652cefae21867f6eafce32408529931710e5c83
Richard Fenix <richard.fenix@gmail.com>
2018-02-21 05:02:24 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,9 +1,6 @@
 
 let playlist = {Katy: "Friday Night"};
 
-function updatePlaylist(playlist, artist, song) {
-  return Object.assign({}, playlist, {[artist]: song})
-}
 
 function removeFromPlaylist(playlist, artist){
   delete playlist[artist];
